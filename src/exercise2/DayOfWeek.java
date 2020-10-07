2d65bffa215c50c523ca3e2caf288bcfae6da175
AntoninaY01 <a.yordanova01@icloud.com>
2020-10-07 15:35:25 +0000

    kod

---
+++ src/exercise2/DayOfWeek.java
@@ -0,0 +1,28 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int number = input.nextInt();
+  
+  if(number==1){
+    System.out.println("Day of Week: Monday");
+  }else if(number==2){
+    System.out.println("Day of Week: Tuesday");
+  } if(number==3){
+    System.out.println("Day of Week: Wednesday");
+  }else if(number==4){
+    System.outprintln("Day of Week: Thursday");
+  } if(number==5){
+    System.out.println("Day of Week: Friday");
+  }else if(number==6){
+    System.out.println("Day of Week: Saturday");
+  }
+
+  
+ 
+}
