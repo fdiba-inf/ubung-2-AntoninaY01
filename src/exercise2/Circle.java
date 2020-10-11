4c7e3e8e606f142de514f4a9223043dde3c86393
AntoninaY01 <a.yordanova01@icloud.com>
2020-10-11 21:15:49 +0000

    a

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -6,15 +6,15 @@ public class Circle {
  
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter the Radius");
+    
 
 
    double R = input.nextInt();
    double C = 2*3.14*R;
    double A = 3.14*R*R;
 
-   System.out.println("Circumference:" + C);
-   System.out.println("Area:" + A);
+   System.out.println("Circumference: " + C);
+   System.out.println("Area: " + A);
 
 
 
